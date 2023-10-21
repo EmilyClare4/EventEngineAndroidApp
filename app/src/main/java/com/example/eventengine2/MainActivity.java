8031d6e48cd74bf61b4c9efa2c437d9eb8aeeb54
Emily <emily_clare@live.com>
2023-10-21 15:39:31 +0200

    Spinnder works correctly

--- app/src/main/java/com/example/eventengine2/MainActivity.java
+++ app/src/main/java/com/example/eventengine2/MainActivity.java
@@ -35,9 +35,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mMainLayout=ActivityMainBinding.inflate(getLayoutInflater());
+        mMainLayout = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mMainLayout.getRoot());
-
 
         // Initialize the EventDatabase instance
         Log.d("MyApp", "Database context: " + this);
@@ -47,103 +46,42 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         // Replace this with actual category data retrieval logic
         getCategoryDataFromDatabase();
-
-        //adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
-        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        //categorySpinner.setAdapter(adapter);
-
-       /* categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                // Handle the selected category here
-                String selectedCategory = (String) parentView.getItemAtPosition(position);
-                if (selectedCategory != "Select a category from the list") {
-                    // Create an Intent to start the EventListActivity
-                    Intent intent = new Intent(MainActivity.this, EventListActivity.class);
-                    intent.putExtra("selectedCategory", selectedCategory);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(MainActivity.this, "Please select a category.", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });*/
     }
 
     private void getCategoryDataFromDatabase() {
         EventDatabase.runOnDatabaseExecutor(() -> {
             List<Category> categories = categoryDao.getAllCategories();
-            List<Event> events = eventDao.getAllEvents();
             Log.d("MyApp", "Categories retrieved: " + categories.size());
-            Log.d("MyApp", "Events retrieved: " + events.size());
             List<String> categoryNames = new ArrayList<>();
+            // Add the "Select a category" as the initial item
+            categoryNames.add("Select a category");
             for (Category category : categories) {
                 categoryNames.add(category.getName());
             }
             mMainLayout.categorySpinner.post(() -> {
                 adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, categoryNames);
                 mMainLayout.categorySpinner.setAdapter(adapter);
-                mMainLayout.categorySpinner.setSelection(mSelectedCategory);
-                mMainLayout.categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        String selectedCategory = categoryNames.get(position);
-                        if (!selectedCategory.equals("Select a category from the list")) {
-                            // Create an Intent to start the EventListActivity
-                            Intent intent = new Intent(MainActivity.this, EventListActivity.class);
-                            intent.putExtra("selectedCategory", selectedCategory);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(MainActivity.this, "Please select a category.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-
-                    }
-                });
+                mMainLayout.categorySpinner.setSelection(0); // Select the initial item
+                mMainLayout.categorySpinner.setOnItemSelectedListener(this); // Set the listener
             });
         });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        //updateCategoryList();
-        Toast.makeText(MainActivity.this, "Please select a category.", Toast.LENGTH_SHORT).show();
+        String selectedCategory = parent.getItemAtPosition(position).toString();
+        if (!selectedCategory.equals("Select a category")) {
+            // Create an Intent to start the EventListActivity
+            Intent intent = new Intent(MainActivity.this, EventListActivity.class);
+            intent.putExtra("selectedCategory", selectedCategory);
+            startActivity(intent);
+        } else {
+            Toast.makeText(MainActivity.this, "Please select a category.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-
-
-
-
-       /* // Use AsyncTask to retrieve categories from the Category table
-        new AsyncTask<Void, Void, List<Category>>() {
-            @Override
-            protected List<Category> doInBackground(Void... voids) {
-                return eventDatabase.
-            }
-
-            @Override
-            protected void onPostExecute(List<Category> categoryList) {
-                for (Category category : categoryList) {
-                    categories.add(category.getName());
-                }
-
-                // Update the spinner adapter when data is available
-                adapter.notifyDataSetChanged();
-            }
-        }.execute();
-
-        return categories;
-    }*/
 }
